fb7f6bfd47bbd1f3af7136d8fdf9cbc1f7fb8f40
Himumi <erliansyaha@gmail.com>
2024-02-07 07:14:06 +0900

    Refactor - Change display to use class variable

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,17 +1,18 @@
 class Game
+    @@board = [1,2,3,4,5,6,7,8,9]
   def initialize
     @fist_player = "X"
     @last_player = "O"
   end
 
-  def display(board)
-    puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  def display
+    puts "#{@@board[0]} | #{@@board[1]} | #{@@board[2]}"
     puts "---------"
-    puts "#{board[3]} | #{board[4]} | #{board[5]}"
+    puts "#{@@board[3]} | #{@@board[4]} | #{@@board[5]}"
     puts "---------"
-    puts "#{board[6]} | #{board[7]} | #{board[8]}"
+    puts "#{@@board[6]} | #{@@board[7]} | #{@@board[8]}"
   end
 end
 
 game = Game.new
-game.display([1,2,3,4,5,6,7,8,9])
+game.display
