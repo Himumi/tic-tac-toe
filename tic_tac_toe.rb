63c50e3fc498397f8c348ab177a321ec8cf2f518
Himumi <erliansyaha@gmail.com>
2024-02-07 07:11:26 +0900

    Build - Create display board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,4 +3,15 @@ class Game
     @fist_player = "X"
     @last_player = "O"
   end
+
+  def display(board)
+    puts "#{board[0]} | #{board[1]} | #{board[2]}"
+    puts "---------"
+    puts "#{board[3]} | #{board[4]} | #{board[5]}"
+    puts "---------"
+    puts "#{board[6]} | #{board[7]} | #{board[8]}"
+  end
 end
+
+game = Game.new
+game.display([1,2,3,4,5,6,7,8,9])
