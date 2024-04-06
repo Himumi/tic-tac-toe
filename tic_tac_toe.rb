b7559d2d8c88809bf47adfeb601855a47a6aa7de
Himumi <erliansyaha@gmail.com>
2024-04-06 17:28:00 +0900

    refactor: change all class variables into instance variables

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,31 +1,33 @@
 class Game
-  @@board = [1,2,3,4,5,6,7,8,9]
+
+  def initialize
+    @board = [1,2,3,4,5,6,7,8,9]
   # @@board = ["X","O","X","O","O","X", "X", "X", "O"]
-  @@win_combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [3, 5, 8], [0, 4, 8], [2, 4, 6]]
-  @@first_player = "X"
-  @@last_player = "O"
-  @@winner
+    @win_combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [3, 5, 8], [0, 4, 8], [2, 4, 6]]
+    @first_player = "X"
+    @last_player = "O"
+  end
 
   def display
-    puts "#{@@board[0]} | #{@@board[1]} | #{@@board[2]}"
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
     puts "---------"
-    puts "#{@@board[3]} | #{@@board[4]} | #{@@board[5]}"
+    puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
     puts "---------"
-    puts "#{@@board[6]} | #{@@board[7]} | #{@@board[8]}"
+    puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
   end
 
   def add_to_display(index, player)
-    @@board[index] = player
+    @board[index] = player
   end
 
   def current_player(count)
-    count.odd? ? @@first_player : @@last_player
+    count.odd? ? @first_player : @last_player
   end
 
   def is_taken(index)
     # ask type of date in @@board[index]
     # @@board[index].is_a? String
-    @@board[index].class == String
+    @board[index].class == String
   end
 
   def valid_input(input)
@@ -44,23 +46,23 @@ class Game
 
   def is_won
     # @@win_combinations.any? do |item|
-    #   item.all? do |i| 
+    #   item.all? do |i|
     #   @@winner = @@board[i]
     #   @@board[i] == @@first_player || @@board[i] == @@last_player
     #   end
     # end
 
-    @@win_combinations.any? do |item|
-      value_1 = @@board[item[0]]
-      value_2 = @@board[item[1]]
-      value_3 = @@board[item[2]]
-      @@winner = @@board[item[0]]
+    @win_combinations.any? do |item|
+      value_1 = @board[item[0]]
+      value_2 = @board[item[1]]
+      value_3 = @board[item[2]]
+      @winner = @board[item[0]]
       value_1 == value_2 and value_2 == value_3
     end
   end
 
   def is_full
-    @@board.all? { |item| item.class != Integer }
+    @board.all? { |item| item.class != Integer }
   end
 
   def is_draw
@@ -72,7 +74,7 @@ class Game
   end
 
   def message
-    is_won ? "The winner is #{@@winner}! Congratulations!!!" : "Game over!!!"
+    is_won ? "The winner is #{@winner}! Congratulations!!!" : "Game over!!!"
   end
 
   def play
@@ -83,7 +85,7 @@ class Game
       display
       count += 1
     end
-     puts message
+    puts message
   end
 end
 
@@ -95,4 +97,3 @@ puts "---------"
 puts "7 | 8 | 9"
 game = Game.new
 puts game.play
-
