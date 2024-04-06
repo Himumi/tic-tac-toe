0afc418abc821a2411527f6adf4b3951a142e0ca
Himumi <erliansyaha@gmail.com>
2024-04-06 20:51:32 +0900

    test: valid_input method when input valid value

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,4 +2,14 @@ require './lib/tic_tac_toe'
 
 describe Game do
   subject(:game) { described_class.new }
+
+  describe '#valid_input' do
+    context 'when user inputs valid value' do
+      it 'returns true' do
+        value = 5
+        check_value = game.valid_input(value)
+        expect(check_value).to be true
+      end
+    end
+  end
 end
